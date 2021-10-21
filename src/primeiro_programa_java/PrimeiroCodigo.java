5b244eafb558f2835d9174be9ecde0cf5a12fa93
Henrique Miam <henriquemiam02@gmail.com>
2021-10-21 11:36:32 -0300

    first commit

---
+++ src/primeiro_programa_java/PrimeiroCodigo.java
@@ -0,0 +1,13 @@
+package primeiro_programa_java;
+
+public class PrimeiroCodigo {
+
+	public static void main(String[] args) {
+		
+        Gato gato = new Gato ("Lilica" , "branca" , 2 );
+       
+        System.out.println(gato);
+	}
+
+	
+}
